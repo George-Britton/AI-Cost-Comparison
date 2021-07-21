4763fb9fa490856297d0e5f422e793f33052856c
George Britton <george.britton97@hotmail.com>
2021-07-21 17:24:49 +0100

    Started a GOAP manager

---
+++ Source/DissProject/Characters/GOAPEnemyManager.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GOAPEnemyManager.h"
+
+// Sets default values
+AGOAPEnemyManager::AGOAPEnemyManager()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AGOAPEnemyManager::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AGOAPEnemyManager::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
